b312943b0ea92890a27388848ecc877ba6b02000
zhouger <2112581537@qq.com>
2024-02-07 15:47:38 +0800

    工资账套接口

--- OA_server/src/main/java/com/example/server/controller/SalaryController.java
+++ OA_server/src/main/java/com/example/server/controller/SalaryController.java
@@ -1,8 +1,15 @@
 package com.example.server.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.server.pojo.RespBean;
+import com.example.server.pojo.Salary;
+import com.example.server.service.ISalaryService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * <p>
@@ -13,7 +20,59 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2023-03-30
  */
 @RestController
-@RequestMapping("/salary")
+@RequestMapping("/salary/sob")
 public class SalaryController {
+
+    @Autowired
+    private ISalaryService salaryService;
+
+
+    @ApiOperation(value = "获取所有工资账套")
+    @GetMapping("/getAllSalaries")
+    //获取所有工资账套
+    public List<Salary> getAllSalaries()
+    {
+        return salaryService.list();
+    }
+
+
+    @ApiOperation(value = "添加员工账套")
+    @PostMapping("/")
+    public RespBean addSalary(@RequestBody Salary salary)
+    {
+        salary.setCreateDate(LocalDateTime.now());//设置创建时间
+        if (salaryService.save(salary))
+        {
+            return RespBean.success("添加成功");
+        }else
+        {
+            return RespBean.error("添加失败，请重试");
+        }
+
+    }
+
+    @ApiOperation(value = "删除工资账套")
+    @DeleteMapping("/{id}")
+    //根据前端传入id删除工资账套
+    public RespBean deleteSalary(@PathVariable Integer id)
+    {
+        if (salaryService.removeById(id))
+        {
+            return RespBean.success("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
+
+    @ApiOperation(value = "更新工资账套")
+    @PutMapping("/")
+    public RespBean updateSalary(@RequestBody Salary salary)
+    {
+        if (salaryService.updateById(salary))
+        {
+            return RespBean.success("更新成功");
+        }
+        return RespBean.error("更新失败");
+    }
+
 
 }
---
+++ OA_server/src/main/java/com/example/server/controller/SalarySobCfgController.java
@@ -0,0 +1,9 @@
+package com.example.server.controller;
+
+/**
+ * @author cjz
+ * @date 2024/2/7 15:33
+ * @description:
+ */
+public class SalarySobCfgController {
+}
